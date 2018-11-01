9f0c3120d43686fd18622ff61c8f08bc70113d2e
Aisha Toulegenova <aetoule@gmail.com>
2018-10-31 20:53:38 -0700

    changed port

--- server/index.js
+++ server/index.js
@@ -174,7 +174,7 @@ app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
 })
 
-const PORT = 5432;
+const PORT = 5100;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} `);
+  console.log(`Server listening on port ${5100} `);
 });
